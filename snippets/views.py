2eef22fba0ff91ec8e56c3a899ca0cacefd01deb
CoreyVincent <corey.vincent@live.com>
2018-05-07 13:33:29 -0400

    updating files

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,7 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -9,6 +10,14 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 # from snippets.models import Snippet
 # from snippets.serializers import SnippetSerializer
